8839703db2ab075e14cff9e53ee3381040db8643
Oleg Shtein <olegshteyn@gmail.com>
2026-02-14 23:17:02 +0700

    feat: add AdvantagesSection component template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
+import { AdvantagesSection } from './components/AdvantagesSection/AdvantagesSection'
 import './styles'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <AdvantagesSection />
     </>
   )
 }
---
+++ src/components/AdvantagesSection/AdvantagesSection.tsx
@@ -0,0 +1,14 @@
+import styles from './AdvantagesSection.module.css'
+
+export const AdvantagesSection = () => {
+  return (
+    <section className={styles.advantagesSection}>
+      <div className={styles.title}>
+
+      </div>
+      <div className={styles.advantagesList}>
+
+      </div>
+    </section>
+  )
+}
