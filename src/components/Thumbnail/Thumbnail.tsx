c0d7fdd30ec32b0c2f5e75a3640229234a3a165f
Oleg Shtein <olegshteyn@gmail.com>
2026-02-17 14:33:53 +0700

    feat: add Thumbnail component

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,14 +1,21 @@
 import type { Props } from './Thumbnail.types'
+import styles from './Thumbnail.module.css'
 
 export const Thumbnail = (props: Props) => {
   const { imageName } = props
 
+  const imagePath = `/src/assets/images/${imageName}`
+
   return (
-    <div>
-      <img
-        src={imageName}
-        alt=""
-      />
+    <div className={styles.thumbnail}>
+      <div className={styles.thumbnailInner}>
+        <img
+          width={40}
+          height={40}
+          src={imagePath}
+          alt=""
+        />
+      </div>
     </div>
   )
 }
