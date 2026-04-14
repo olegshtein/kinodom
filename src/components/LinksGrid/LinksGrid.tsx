266dfda6332e7b2dfdbc85fe3d9824b5f2849a3e
Oleg Shtein <olegshteyn@gmail.com>
2026-04-14 19:12:55 +0700

    Добавлены стили для блока ссылок в футере

--- src/components/LinksGrid/LinksGrid.tsx
+++ src/components/LinksGrid/LinksGrid.tsx
@@ -4,13 +4,15 @@ import styles from "./LinksGrid.module.css";
 export const LinksGrid = () => {
   return (
     <div className={styles.linksGrid}>
-      {footerLinksGridContent.cells.map(({ label, links }) => (
-        <div className={styles.cell}>
+      {footerLinksGridContent.cells.map(({ key, mod, label, links }) => (
+        <div className={`${styles.cell} ${styles[mod]}`} key={key}>
           <div className={styles.label}>{label}</div>
           <ul className={styles.linksList}>
-            {links.map(({ href, anchor }) => (
-              <li>
-                <a href={href}>{anchor}</a>
+            {links.map(({ key, href, anchor }) => (
+              <li key={key}>
+                <a className={styles.link} href={href} target="_blank">
+                  {anchor}
+                </a>
               </li>
             ))}
           </ul>
--- src/config/footerLinksGrid.ts
+++ src/config/footerLinksGrid.ts
@@ -1,47 +1,59 @@
+import { nanoid } from "nanoid";
+
 export const footerLinksGridContent = {
   cells: [
     {
+      key: nanoid(),
+      mod: "menu",
       label: "Меню",
       links: [
-        { href: "", anchor: "Фильмы" },
-        { href: "", anchor: "Сериалы" },
-        { href: "", anchor: "Подборки" },
+        { key: nanoid(), href: "", anchor: "Фильмы" },
+        { key: nanoid(), href: "", anchor: "Сериалы" },
+        { key: nanoid(), href: "", anchor: "Подборки" },
       ],
     },
     {
+      key: nanoid(),
+      mod: "moviesAndSeries",
       label: "Кино и сериалы",
       links: [
-        { href: "", anchor: "Весь каталог" },
-        { href: "", anchor: "Комедии" },
-        { href: "", anchor: "Драмы" },
-        { href: "", anchor: "Фэнтези" },
-        { href: "", anchor: "Триллеры" },
-        { href: "", anchor: "Детективы" },
+        { key: nanoid(), href: "", anchor: "Весь каталог" },
+        { key: nanoid(), href: "", anchor: "Комедии" },
+        { key: nanoid(), href: "", anchor: "Драмы" },
+        { key: nanoid(), href: "", anchor: "Фэнтези" },
+        { key: nanoid(), href: "", anchor: "Триллеры" },
+        { key: nanoid(), href: "", anchor: "Детективы" },
       ],
     },
     {
+      key: nanoid(),
+      mod: "cooperation",
       label: "Сотруднечество",
       links: [
-        { href: "", anchor: "Размещение рекламы" },
-        { href: "", anchor: "Партнерам" },
-        { href: "", anchor: "Партнерская программы" },
-        { href: "", anchor: "Пользовательское соглашение" },
-        { href: "", anchor: "Политика конфиденциальности" },
+        { key: nanoid(), href: "", anchor: "Размещение рекламы" },
+        { key: nanoid(), href: "", anchor: "Партнерам" },
+        { key: nanoid(), href: "", anchor: "Партнерская программы" },
+        { key: nanoid(), href: "", anchor: "Пользовательское соглашение" },
+        { key: nanoid(), href: "", anchor: "Политика конфиденциальности" },
       ],
     },
     {
+      key: nanoid(),
+      mod: "support",
       label: "Техническая поддержка",
       links: [
-        { href: "", anchor: "8 800 800 80 80" },
-        { href: "", anchor: "support@main.ru" },
+        { key: nanoid(), href: "", anchor: "8 800 800 80 80" },
+        { key: nanoid(), href: "", anchor: "support@main.ru" },
       ],
     },
     {
+      key: nanoid(),
+      mod: "subscribe",
       label: "Подписывайтесь на нас",
       links: [
-        { href: "", anchor: "facebook" },
-        { href: "", anchor: "vk" },
-        { href: "", anchor: "instagram" },
+        { key: nanoid(), href: "", anchor: "facebook" },
+        { key: nanoid(), href: "", anchor: "vk" },
+        { key: nanoid(), href: "", anchor: "instagram" },
       ],
     },
   ],
